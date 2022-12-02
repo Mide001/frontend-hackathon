3198bfb98835f3fae1999248b6912f928e79b73c
Mide001 <olamideisreal5@gmail.com>
2022-12-02 15:17:56 +0100

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,61 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import img2 from "../src/assets/nft1.webp";
+import img1 from "../src/assets/nft2.webp";
+import img3 from "../src/assets/nft3.webp";
+import image from "../src/assets/ArtBloc.jpg";
+import Link from "next/link";
 
-export default function Home() {
-  return (
+function Home() {
+  return(
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
+      <div className={styles.main}>
+        <main className={styles.hero}>
+          <div className={styles.section}>
+            <h2>Mint, Buy & Trade Fractional NFTs</h2>
+            <span>
+              Mint NFT as a proof for your digital artwork in simple steps and sell it in form of fractions directly on the platform to earn from it. Token Holders earn a profit by holding the token for a period of time
+            </span>
+            <div className={styles.buttons}>
+              <button className={styles.btn}>
+                <Link href={"/create"}>Create Listing</Link>
+              </button>
+              <button className={styles.btn2}>
+                {" "}
+                <Link href={"/marketplace"}>Marketplace</Link> 
+              </button>
+            </div>
+          </div>
+          <div className={styles.image}>
+            <Image className={styles.image} src={image} />
+          </div>
+        </main>
+        <h2>Platform Features</h2>
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
+        <div className={styles.collection}>
+          <div className={styles.feature}>
+            All assets stored on decentralized storage like IPFS{" "}
+          </div>
+          <div className={styles.feature}>
+            Own a art piece, NFT just for fractions of the cost{" "}
+          </div>
+          <div className={styles.feature}>
+            Earn a wholesome profit from holding tokens for a short period of time
+          </div>
+          <div className={styles.feature}>
+            Mint Digital Proof as NFT for your Artwork for free in under 60 secs
+          </div>
+          <div className={styles.feature}>
+            Control the Sale of NFT and tokens for your artwork
+          </div>
+          <div className={styles.feature}>
+            Buy tokens/fractions of the NFTs and NFTs at the same platform
+          </div>
         </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
+
+export default Home;
